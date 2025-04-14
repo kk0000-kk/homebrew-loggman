698957f9089cba58e77a8f03cf7eee532952d32b
K.Kobayashi <kobayashi@sonicgarden.jp>
2025-04-14 23:34:18 +0900

    v0.1.0

--- loggman.rb
+++ loggman.rb
@@ -1,12 +1,9 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Loggman < Formula
   desc "log tool"
   homepage "https://github.com/kk0000-kk/loggman"
-  url "https://github.com/kk0000-kk/loggman/releases/download/v0.0.3/loggman"
-  sha256 "004b2df785a8446ea72f00805727654fc54938b443faad81f797a4a122744e3d"
-  license "BSD-3-Clause"
+  url "https://github.com/kk0000-kk/loggman/releases/download/v0.1.0/loggman"
+  sha256 "0bb5e4da97fa534fb4aa8fbeb21718fbddc6f953e8297f5b31ec9cdbb4651e45"
+  license "MIT"
 
   def install
     bin.install "loggman"
