a39e0f742aa0c149cef3f9515106def837394e92
K.Kobayashi <kobayashi@sonicgarden.jp>
2025-04-18 22:49:02 +0900

    v0.1.1

--- loggman.rb
+++ loggman.rb
@@ -1,8 +1,8 @@
 class Loggman < Formula
   desc "log tool"
   homepage "https://github.com/kk0000-kk/loggman"
-  url "https://github.com/kk0000-kk/loggman/releases/download/v0.1.0/loggman"
-  sha256 "0bb5e4da97fa534fb4aa8fbeb21718fbddc6f953e8297f5b31ec9cdbb4651e45"
+  url "https://github.com/kk0000-kk/loggman/releases/download/v0.1.1/loggman"
+  sha256 "91ab8b7671ac8e12c90990a3e84adfa6a086345dfa7f62b79856d194afe0ade9"
   license "MIT"
 
   def install
